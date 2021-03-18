fddc1d840ed0abd3578a6f9cd34f8da7cf61241b
Dmitrii Frolov <dmitriifrolov@Dmitriis-MacBook-Pro.local>
2021-03-18 21:21:26 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser)
+
+server.post('lists/:id/tasks', (req, res)=>{
+  res.jsonp
+})
+
+server.use(router)
+server.use(jsonServer.rewriter({
+  "/lists/:id/tasks": "/tasks?listsId=:id"
+}))
+server.listen(process.env.PORT || 3000, ()=>{
+  console.log(`app is running on port ${process.env.PORT}`)
+})
